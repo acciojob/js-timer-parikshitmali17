0df9a5318e3a8de5feb5c75540592de287adb6b5
Parikshit Mali <parikshitmali17@gmail.com>
2025-03-27 17:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // write js code here if required
+
+let p=document.getElementById("timer")
+
+let timer=new Date()
+
+p.innerText=`${timer.getDate()}/${timer.getMonth()}/${timer.getFullYear()},${timer.getHours()}:${timer.getMinutes()}:${timer.getMilliseconds()}`
